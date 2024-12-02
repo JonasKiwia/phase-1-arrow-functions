fd77353a237a7f9a561ca3595cfda9e8a4ffbfe0
JonasKiwia <jonas.kiwia@student.moringaschool.com>
2024-12-02 16:37:44 +0000

    Implement divide, square, and add functions

---
+++ index.js
@@ -0,0 +1,10 @@
+// Function for divide
+const divide = function () {
+  return 2000 / 100;
+};
+
+// Arrow function square
+const square = (num) => num * num;
+
+// Arrow function add
+const add = (a, b) => a + b;
